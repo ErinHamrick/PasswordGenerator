7079b6439ea79e2cd9eb123ca2e8d8061437d3bf
Erin Hamrick <erin.hamrick76@gmail.com>
2023-12-21 16:08:54 -0600

    refactored logic so it would not return undefined if no option is chosen

--- assets/script.js
+++ assets/script.js
@@ -1,95 +1,95 @@
 // access to generate button
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 // character arrays
 var lowercase = [
-	"a",
-	"b",
-	"c",
-	"d",
-	"e",
-	"f",
-	"g",
-	"h",
-	"i",
-	"j",
-	"k",
-	"l",
-	"m",
-	"n",
-	"o",
-	"p",
-	"q",
-	"r",
-	"s",
-	"t",
-	"u",
-	"v",
-	"w",
-	"x",
-	"y",
-	"z",
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
 ];
 
 var uppercase = [
-	"A",
-	"B",
-	"C",
-	"D",
-	"E",
-	"F",
-	"G",
-	"H",
-	"I",
-	"J",
-	"K",
-	"L",
-	"M",
-	"N",
-	"O",
-	"P",
-	"Q",
-	"R",
-	"S",
-	"T",
-	"U",
-	"V",
-	"W",
-	"X",
-	"Y",
-	"Z",
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
 ];
 
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 var special = [
-	"#",
-	"$",
-	"%",
-	"&",
+	'#',
+	'$',
+	'%',
+	'&',
 	"'",
-	"(",
-	")",
-	"*",
-	".",
-	"+",
+	'(',
+	')',
+	'*',
+	'.',
+	'+',
 	'"',
-	"-",
-	"<",
-	">",
-	"?",
-	"^",
-	"`",
-	"{",
-	"|",
-	"}",
-	"~",
-	"\\",
+	'-',
+	'<',
+	'>',
+	'?',
+	'^',
+	'`',
+	'{',
+	'|',
+	'}',
+	'~',
+	'\\',
 ];
 
 // Write password to the #password input
 function writePassword() {
 	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
+	var passwordText = document.querySelector('#password');
 
 	passwordText.value = password;
 }
@@ -106,7 +106,7 @@ function generatePassword() {
 	// Parses string input into integer
 	var passwordLength = parseInt(
 		prompt(
-			"How long would you like your password to be? Must be a number between 8-128;"
+			'How long would you like your password to be? Must be a number between 8-128;'
 		)
 	);
 	// Conditional statement to alert user if conditions are not met
@@ -115,21 +115,21 @@ function generatePassword() {
 		passwordLength > 128 ||
 		Number.isNaN(passwordLength)
 	) {
-		alert("Input must be a number between 8-128");
+		alert('Input must be a number between 8-128');
 		return;
 	}
 	// variables for confirm functions
 	var confirmLowercase = confirm(
-		"Use lowercase letters in your password? Select cancel to opt out."
+		'Use lowercase letters in your password? Select cancel to opt out.'
 	);
 	var confirmUppercase = confirm(
-		"Use uppercase letters in your password? Select cancel to opt out."
+		'Use uppercase letters in your password? Select cancel to opt out.'
 	);
 	var confirmSpecialCharacters = confirm(
-		"Use special characters in your password? Select cancel to opt out."
+		'Use special characters in your password? Select cancel to opt out.'
 	);
 	var confirmNumber = confirm(
-		"Use numbers in your password? Select cancel to opt out."
+		'Use numbers in your password? Select cancel to opt out.'
 	);
 	// empty variables to track options
 	var characterOptions = [];
@@ -159,18 +159,17 @@ function generatePassword() {
 		var specialCharacters = getRandomCharacter(special);
 		finalPassword.push(specialCharacters);
 		confirmations++;
-	} else if (!confirmations) {
-		alert("Must select at least one character type");
-		var passwordText = document.querySelector("#password");
-		passwordText.textContent('')
-
-	
-			}
+	}
+	if (!confirmations) {
+		alert('Must select at least one character type');
+		var passwordText = document.querySelector('#password');
+		passwordText.textContent = '';
+	}
 	// Loops through conditional statements until all conditions are met then stops
 	for (var i = 0; i < passwordLength - confirmations; i++) {
 		finalPassword.push(getRandomCharacter(characterOptions));
 	}
-	return finalPassword.join("");
+	return finalPassword.join('');
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
